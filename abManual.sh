358e99d1a9aa2b15e32ecc9adb941720dc00fc85
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2020-07-08 18:47:51 +0200

    WIP

--- abManual.sh
+++ abManual.sh
@@ -5,4 +5,5 @@ set -o errexit
 port="${1:-8080}"
 log="${2:-ab.log}"
 
-ab  -n 10000 -c 10 -k -H "X-B3-TraceId: 4883117762eb9420" -H "X-B3-SpanId: 4883117762eb9420" http://127.0.0.1:"${port}"/fooManual > "${log}"
+#ab  -n 10000 -c 10 -k -H "X-B3-TraceId: 4883117762eb9420" -H "X-B3-SpanId: 4883117762eb9420" http://127.0.0.1:"${port}"/fooManual > "${log}"
+ab  -t 30 -c 10 -k -H "X-B3-TraceId: 4883117762eb9420" -H "X-B3-SpanId: 4883117762eb9420" http://127.0.0.1:"${port}"/fooManual > "${log}"
--- src/main/java/com/example/sleuth/SleuthWebfluxBenchmarkSampleApplication.java
+++ src/main/java/com/example/sleuth/SleuthWebfluxBenchmarkSampleApplication.java
@@ -1,23 +1,22 @@
 package com.example.sleuth;
 
-import java.util.function.Consumer;
+import java.time.Duration;
 
-import brave.propagation.CurrentTraceContext;
+import brave.propagation.TraceContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.instrument.reactor.SleuthOperators;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import brave.propagation.TraceContext;
-import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
-import reactor.core.publisher.SignalType;
-import reactor.util.context.Context;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import static org.springframework.cloud.sleuth.instrument.reactor.SleuthOperators.doWithThreadLocal;
 
 @SpringBootApplication
 public class SleuthWebfluxBenchmarkSampleApplication {
@@ -27,7 +26,7 @@ public class SleuthWebfluxBenchmarkSampleApplication {
 		// System.setProperty("spring.sleuth.reactor.instrumentation-type", "DECORATE_ON_EACH");
 		// System.setProperty("spring.sleuth.reactor.instrumentation-type", "DECORATE_ON_LAST");
 //		System.setProperty("spring.sleuth.reactor.instrumentation-type", "MANUAL");
-		SpringApplication.run(SleuthWebfluxBenchmarkSampleApplication.class, args);	
+		SpringApplication.run(SleuthWebfluxBenchmarkSampleApplication.class, args);
 	}
 
 }
@@ -37,31 +36,46 @@ class Foo {
 
 	private static final Logger log = LoggerFactory.getLogger(Foo.class);
 
-	@GetMapping("/foo")
-	public Mono<String> foo() {
-		return Mono.just("fooTraced")
-		.doOnEach(signal -> {
-			log.info("Got a request");
-		})
-		.doOnEach(signal -> {
-			TraceContext traceContext = signal.getContext().get(TraceContext.class);
-			Assert.notNull(traceContext, "Context must be set by Sleuth instrumentation");
-			Assert.state(traceContext.traceIdString().equals("4883117762eb9420"), "TraceId must be propagated");
-		});
-	}
-
 	@GetMapping("/fooNoSleuth")
 	public Mono<String> fooNoSleuth() {
 		return Mono.just("fooTraced")
-		.doOnEach(signal -> log.info("Got a request"));
+				.doOnEach(signal -> log.info("Got a request"))
+				.flatMap(s -> Mono.delay(Duration.ZERO, Schedulers.newParallel("foo")).map(aLong -> {
+					log.info("Logging [{}] from flat map", s);
+					return "";
+				}))
+				.doOnEach(signal -> {
+					log.info("Doing assertions");
+					Assert.isTrue(signal.getContext().isEmpty(), "Context must be empty");
+				});
 	}
 
+	@GetMapping("/foo")
+	public Mono<String> foo() {
+		return Mono.just("fooTraced")
+				.doOnNext(signal -> log.info("Got a request"))
+				.flatMap(s -> Mono.delay(Duration.ZERO, Schedulers.newParallel("foo")).map(aLong -> {
+					log.info("Logging [{}] from flat map", s);
+					return "";
+				}))
+				.doOnEach(signal -> {
+					log.info("Doing assertions");
+					TraceContext traceContext = signal.getContext().get(TraceContext.class);
+					Assert.notNull(traceContext, "Context must be set by Sleuth instrumentation");
+					Assert.state(traceContext.traceIdString().equals("4883117762eb9420"), "TraceId must be propagated");
+				});
+	}
 
 	@GetMapping("/fooManual")
 	public Mono<String> fooManual() {
-		return Mono.just("fooManual")
-				.doOnEach(SleuthOperators.doWithThreadLocal(() -> log.info("Got a request")))
+		return Mono.just("fooTraced")
+				.doOnEach(doWithThreadLocal(() -> log.info("Got a request")))
+				.flatMap(s -> Mono.subscriberContext().delayElement(Duration.ZERO, Schedulers.newParallel("foo")).map(ctx -> {
+					doWithThreadLocal(ctx, () -> log.info("Logging [{}] from flat map", s));
+					return "";
+				}))
 				.doOnEach(signal -> {
+					doWithThreadLocal(signal.getContext(), () -> log.info("Doing assertions"));
 					TraceContext traceContext = signal.getContext().get(TraceContext.class);
 					Assert.notNull(traceContext, "Context must be set by Sleuth instrumentation");
 					Assert.state(traceContext.traceIdString().equals("4883117762eb9420"), "TraceId must be propagated");
